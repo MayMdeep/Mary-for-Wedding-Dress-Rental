d2fed9b6e19282e623c3849f9136c8d2d99e3ab3
MayDeep <MayDeep@winshakes.com>
2024-09-05 20:54:03 +0300

    remove balances apis

--- routes/api.php
+++ routes/api.php
@@ -113,7 +113,3 @@ Route::group(['prefix' => 'reservations'], function () {
     Route::delete('{id}', DeleteReservationAction::class);
     Route::put('', StoreReservationAction::class);
 });
-Route::group(['prefix'=>'balances'], function(){
-    Route::put('', StoreBalanceAction::class);
-    Route::get('', GetBalanceListByUserAction::class);
-});
